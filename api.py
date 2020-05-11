8c73e25ae3550c6cf65b7ddfaeb7534cc5b93d26
Miguel Angel <posadasarraez@gmail.com>
2020-05-11 12:41:24 +0200

    Añadidas sesiones con beaker

--- api.py
+++ api.py
@@ -1,10 +1,48 @@
-from bottle import run,route, template, get, post, request, delete
+from bottle import *
+import bottle_session
+import bottle
+import beaker.middleware
 
-users = [{'name' : 'illomigue', 'type' : 'superuser'},
-          {'name' : 'd3vcho', 'type' : 'superuser'},
-          {'name' : 'Direkk', 'type' : 'standard'},
-          {'name' : 'corderop', 'type' : 'standard'},
-          {'name' : 'currobeltran', 'type' : 'standard'}]
+session_opts = {
+    'session.type': 'file',
+    'session.data_dir': './.session/',
+    'session.auto': True,
+}
+
+app = beaker.middleware.SessionMiddleware(bottle.app(), session_opts)
+
+
+
+users = [{'name' : 'illomigue', 'type' : 'superuser','password' : '1234' },
+          {'name' : 'd3vcho', 'type' : 'superuser','password' : '1234'},
+          {'name' : 'Direkk', 'type' : 'standard','password' : '1234'},
+          {'name' : 'corderop', 'type' : 'standard','password' : '1234'},
+          {'name' : 'currobeltran', 'type' : 'standard','password' : '1234'}]
+
+
+#Antes de atender cualquier peticion, almacenamos la sesion de beaker en request.session, es el equivalente a session_start() que utilizamos en php
+@hook('before_request')
+def setup_request():
+    request.session = request.environ['beaker.session']
+
+
+@get('/')
+def index():
+    if 'logued' in request.environ['beaker.session']:
+        if request.environ['beaker.session']['logued'] == False :
+            return template('views/index')
+        else:
+            for user in users:
+                if user['name'] == request.session['username']:
+                    logued = user
+            return template('views/usuario',logued=logued)
+    else:
+        return template('views/index')
+
+#Enlazar el css
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='views/css/')
 
 
 
@@ -26,9 +64,27 @@ def getUser(name):
 #Esta funcion recibe un objeto en formato JSON
 @post('/users')
 def addUser():
-    new_user = {'name' : request.json.get('name'), 'type' : request.json.get('type')}
+    new_user = {'name' : request.forms.get('name'), 'type' : request.forms.get('type')}
     users.append(new_user)
     return {'users' : users}
+
+@post('/login')
+def login():
+    log_user = {'name' : request.forms.get('name'), 'password' : request.forms.get('password')}
+    for user in users:
+        if user['name'] == log_user['name'] and user['password'] == log_user['password'] :
+            logued = user
+            request.environ['beaker.session']['username'] = log_user['name']
+            request.environ['beaker.session']['logued'] = True 
+            return template('views/usuario',logued=logued)
+        
+    return '404 not found'
+
+
+@post('/logout')
+def logout():
+    request.environ['beaker.session'].delete()
+    return template('views/index')
 
 
 
@@ -45,10 +101,9 @@ def deleteUser(name):
     return {'users' : users}
 
 
-    
 
-if __name__ == '__main__':
-    run(debug=True, reloader=True)
+
+bottle.run(app=app,debug=True, reloader=True)
 
 
 
