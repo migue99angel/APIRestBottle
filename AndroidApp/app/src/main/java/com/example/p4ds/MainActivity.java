a05c6333604055ddd0c3a525949103c0ad53bbef
frandominguez03 <d3vcho@protonmail.com>
2020-05-21 13:06:34 +0200

    Avanzando en Android

--- AndroidApp/app/src/main/java/com/example/p4ds/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/p4ds/MainActivity.java
@@ -10,11 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -39,35 +35,51 @@ public class MainActivity extends AppCompatActivity {
     public void iniciarSesion(View view) {
         String url = "http://10.0.2.2:5000/login"; // Hay que usar esta IP para referirnos a nuestra propia máquina y no al propio emulador
 
-        TextView name = (TextView) findViewById(R.id.textView2);
-        TextView password = (TextView) findViewById(R.id.textView3);
+        final EditText name = (EditText) findViewById(R.id.editText2);
+        EditText password = (EditText) findViewById(R.id.editText3);
 
-        OkHttpClient client = new OkHttpClient();
+        final OkHttpClient client = new OkHttpClient();
 
         RequestBody cuerpo = new FormBody.Builder()
                 .add("name", name.getText().toString())
                 .add("password", password.getText().toString())
                 .build();
 
-        Request request = new Request.Builder()
+        final Request request = new Request.Builder()
                 .url(url)
                 .post(cuerpo)
                 .build();
 
-        try(Response response = client.newCall(request).execute()) {
-            if(!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        // Con .enqueue hacemos peticiones asíncronas
+        // https://stackoverflow.com/questions/39440806/android-okhttpclient-requesting-error?rq=1
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
-            // Si la consulta tiene éxito, cargamos el perfil del usuario
-            this.cargarPerfil(name.getText().toString());
-        } catch (ClassNotFoundException | SQLException | IOException e) {
-            e.printStackTrace();
-        }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                // Si la consulta tiene éxito, cargamos el perfil del usuario
+                try {
+                    cargarPerfil(name.getText().toString());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void cargarPerfil(String name) throws SQLException, ClassNotFoundException {
         // Consulta para obtener los datos del usuario para cargar el perfil
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://10.0.2.2:8080/P3DS", "usuario", "pass"); // !! CAMBIAR DATOS PARA LA CONEXIÓN !!
+        Connection conn = DriverManager.getConnection("jdbc:mysql://10.0.2.2:8080/p3ds", "usuario", "pass"); // !! CAMBIAR DATOS PARA LA CONEXIÓN !!
         Statement stmt = conn.createStatement();
         String query = "SELECT * FROM usuarios WHERE user=" + name;
         ResultSet rs = stmt.executeQuery(query);
