ad4df9ce6a3a62034d5d3cfdc6a9cccaf97e72e2
Miguel Angel <posadasarraez@gmail.com>
2020-05-23 23:04:32 +0200

    Eliminando funcion innecesaria

--- Usuario.py
+++ Usuario.py
@@ -8,11 +8,6 @@ class Usuario:
         self.amigos = []
         self.seguidores = []
 
-        
-
-    # def cargarPublicaciones(self,publicaciones):
-    #     for p in publicaciones:
-    #         self.publicaciones.append(Publicacion(p[2],p[4],p[3],p[1]))
 
     def cargarPublicacion(self, publicacion, comentarios):
         p = Publicacion(publicacion[2],publicacion[4],publicacion[3],publicacion[1])
